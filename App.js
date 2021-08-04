4caf3598b611218d76c59e512a65cf9525e4006c
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2021-08-04 16:38:05 +0300

    updates

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { store } from './store';
 import HomeScreen from './screens/HomeScreen'
+import MapScreen from './screens/MapScreen'
+import EatsScreen from "./screens/EatsScreen";
 
 export default function App() {
+  const Stack = createNativeStackNavigator()
+
   return (
     <Provider store={store} >
-      <HomeScreen />
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="EatsScreen"
+              component={EatsScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
